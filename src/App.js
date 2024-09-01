27754565d630b162512daef3a6dd58334da85187
vivek <vivekbishnoi26401@gmail.com>
2024-09-01 18:03:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header"; // Import the Header component
+import Home from "./components/Home"; // Import the Home component
+import "./assets/styles/todo.css";
+// import About from "./components/About";
+// import ContactUs from "./components/ContactUs";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header /> {/* Include Header component here */}
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home component as the landing page */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/contact-us" element={<ContactUs />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+ const Header = ()=>{
+
+    // return (
+    //       <div className='App'>
+    //         <h1>Todos</h1>
+    //         <div className='todo-wrapper'>
+    //             <div className='todo-input'>
+    //                 <div className='todo-input-item'>
+    //                     <label>Title</label>
+    //                     <input type='text' placeholder="What's the task title"/>
+    //                 </div>
+    //                 <div className='todo-input-item'>
+    //                     <label>Description</label>
+    //                     <input type='text' placeholder="What's the task description"/>
+    //                 </div>
+    //                 <div className='todo-input-item'>
+    //                     <button type='button' className='primaryBtn'>Add</button>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
+}
+export default Header;
